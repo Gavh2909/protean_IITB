290b2245c6888abd2bf2052d219bfac4f28e2fe2
Satish Gavhane <satishgavhane2909@gmail.com>
2024-04-29 20:58:36 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container mt-4">
+      
+    </div>
+  );
+}
+
+export default App;
